0727501d9f6f65a50910a7510d97bed8d8c14409
Stanislav-Koloda <stanislav.koloda@oa.edu.ua>
2024-05-12 14:31:19 +0300

    Lab_9

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public interface IFactory
+{
+    void Produce();
+}
+
+// виробляє цивільне обладнання
+public class CivilFactory : IFactory
+{
+    public void Produce()
+    {
+        Console.WriteLine("Producing civil equipment...");
+    }
+}
+
+// Декоратор, який додає можливість виробляти військове обладнання
+public class MilitaryDecorator : IFactory
+{
+    private readonly IFactory _factory;
+
+    public MilitaryDecorator(IFactory factory)
+    {
+        _factory = factory;
+    }
+
+    public void Produce()
+    {
+        _factory.Produce();
+        Console.WriteLine("Producing military equipment...");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IFactory civilFactory = new CivilFactory();
+
+        IFactory militaryFactory = new MilitaryDecorator(civilFactory);
+
+        
+        civilFactory.Produce(); 
+        Console.WriteLine();
+        militaryFactory.Produce(); 
+
+        Console.ReadKey();
+    }
+}
