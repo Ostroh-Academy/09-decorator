a135338889e1c8e80a53e11d98ef83190fb10f95
Юлія Денисюк <yuliia.denysiuk@oa.edu.ua>
2024-05-13 21:26:21 +0300

    Лабораторна робота № 9

---
+++ Denysiuk_decorator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Denysiuk_decorator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Client client = new Client();
+
+        var website = new ConcreteComponent();
+        Console.WriteLine("Client: I get a regular website:");
+        client.ClientCode(website);
+        Console.WriteLine();
+
+        // Оптимізуємо вебсайт для мобільних пристроїв
+        MobileOptimizedDecorator mobileWebsite = new MobileOptimizedDecorator(website);
+        Console.WriteLine("Client: Now I've got a mobile-optimized website:");
+        client.ClientCode(mobileWebsite);
+    }
+}
