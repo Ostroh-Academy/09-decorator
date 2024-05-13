0ddd103160525c3a317bb053e1ceacf5bba1453f
Анастасія Черній <jesikafan@gmail.com>
2024-05-13 20:30:08 +0300

    L9 AChernii

---
+++ LAB 9/Decorator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_9
+{
+    interface Comp
+    {
+        string Operation();
+    }
+
+    class ConcreteComponent : Comp
+    {
+        public string Operation()
+        {
+            return "ConcreteComponent";
+        }
+    }
+    abstract class Decorator : Comp
+    {
+        protected Comp component;
+
+        public Decorator(Comp component)
+        {
+            this.component = component;
+        }
+        public virtual string Operation()
+        {
+            return component.Operation();
+        }
+    }
+    class ConcreteDecoratorA : Decorator
+    {
+        public ConcreteDecoratorA(Comp component) : base(component)
+        {
+        }
+
+        public override string Operation()
+        {
+            return $"ConcreteDecoratorA({base.Operation()})";
+        }
+    }
+    class ConcreteDecoratorB : Decorator
+    {
+        public ConcreteDecoratorB(Comp component) : base(component)
+        {
+        }
+
+        public override string Operation()
+        {
+            return $"ConcreteDecoratorB({base.Operation()})";
+        }
+    }
+
+}
---
+++ LAB 9/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LAB_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comp component = new ConcreteComponent();
+            Console.WriteLine(component.Operation());
+
+            Comp decoratedComponent = new ConcreteDecoratorA(component);
+            Console.WriteLine(decoratedComponent.Operation());
+
+            decoratedComponent = new ConcreteDecoratorB(decoratedComponent);
+            Console.WriteLine(decoratedComponent.Operation());
+        }
+    }
+}
