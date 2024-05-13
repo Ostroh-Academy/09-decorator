cd9b5d58a3ad9dd60188dd66da6fda9d54763846
nesterchuk.iraida03@gmail.com <nesterchuk.iraida03@gmail.com>
2024-05-13 06:27:49 +0300

    Паттерн "Декоратор" дозволяє динамічно додавати нові функції об'єктам, обгортаючи їх у вкладені об'єкти-декоратори. Приклад коду, щоб краще зрозуміти, як працює цей паттерн:
    Уявімо, що у нас є базовий клас і кілька декораторів, які можна додати до цього класу, щоб змінити його функціональність. Наприклад, давайте створимо базовий клас Beverage, який представляє напої, і кілька декораторів для додавання різних характеристик, таких як молоко, цукор і т. д.

---
+++ lab 9/lab 9/Class2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_9
+{
+
+    // Декоратор для додавання молока до напою
+    class MilkDecorator : Beverage
+    {
+        private Beverage _beverage;
+
+        public MilkDecorator(Beverage beverage)
+        {
+            _beverage = beverage;
+        }
+
+        public override string Description => _beverage.Description + ", молоко";
+
+        public override double Cost()
+        {
+            return _beverage.Cost() + 0.50;
+        }
+    }
+
+    // Декоратор для додавання цукру до напою
+    class SugarDecorator : Beverage
+    {
+        private Beverage _beverage;
+
+        public SugarDecorator(Beverage beverage)
+        {
+            _beverage = beverage;
+        }
+
+        public override string Description => _beverage.Description + ", цукор";
+
+        public override double Cost()
+        {
+            return _beverage.Cost() + 0.25;
+        }
+    }
+}
---
+++ lab 9/lab 9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Замовляємо каву з молоком та цукром
+            Beverage coffeeWithMilkAndSugar = new SugarDecorator(new MilkDecorator(new Coffee()));
+
+            // Виводимо опис та ціну напою
+            Console.WriteLine("Напій: " + coffeeWithMilkAndSugar.Description);
+            Console.WriteLine("Ціна: $" + coffeeWithMilkAndSugar.Cost());
+            Console.ReadKey();
+        }
+    }
+}
