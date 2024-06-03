7363ca80c616c7fd1224edfb77beb83b6cea34b4
Viktoriia46 <132668579+Viktoriia46@users.noreply.github.com>
2024-06-03 17:35:46 +0300

    Add files via upload

---
+++ 9.14/Decorators/BurgerDecorator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._14.Decorators
+{
+    public abstract class BurgerDecorator : Burger
+    {
+        protected Burger _burger;
+
+        public BurgerDecorator(Burger burger)
+        {
+            _burger = burger;
+        }
+
+        public virtual double GetCost()
+        {
+            return _burger.GetCost();
+        }
+
+        public virtual string GetDescription()
+        {
+            return _burger.GetDescription();
+        }
+    }
+
+}
---
+++ 9.14/Decorators/CheeseDecorator.cs
@@ -0,0 +1,25 @@
+﻿using _9._14.Burgers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._14.Decorators
+{
+    public class CheeseDecorator : BurgerDecorator
+    {
+        public CheeseDecorator(Burger burger) : base(burger) { }
+
+        public override double GetCost()
+        {
+            return _burger.GetCost() + 1.00; // ціна сиру
+        }
+
+        public override string GetDescription()
+        {
+            return _burger.GetDescription() + ", Cheese";
+        }
+    }
+
+}
