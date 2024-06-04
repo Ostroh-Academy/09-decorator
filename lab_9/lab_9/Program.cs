f2490397ca967624bdea3258c3dda32bd35d33bb
ViktoriaDenyshchuk <92024304+ViktoriaDenyshchuk@users.noreply.github.com>
2024-06-04 21:25:12 +0200

    Add files via upload

---
+++ lab_9/lab_9/Program.cs
@@ -0,0 +1,22 @@
+﻿using lab_9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Створення простого аудіофайлу
+        IAudioFile audioFile = new SimpleAudioFile("song.mp3");
+
+        // Додавання реверберації
+        audioFile = new ReverbDecorator(audioFile);
+
+        // Додавання ехо
+        audioFile = new EchoDecorator(audioFile);
+
+        // Відтворення аудіофайлу з ефектами
+        audioFile.Play();
+
+        // Вихід з програми
+        Console.ReadKey();
+    }
+}
