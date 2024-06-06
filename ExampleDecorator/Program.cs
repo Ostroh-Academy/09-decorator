0c52b5d924f64f799a5387dd1f3585ab31b0140b
AlexPrograms <olexiy2004@gmail.com>
2024-06-06 17:27:08 +0200

    lab9

---
+++ ExampleDecorator/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace ExampleDecorator;
+
+// The base Component interface defines operations that can be altered by
+// decorators.
+public abstract class Component
+{
+    public abstract string Operation();
+}
+
+// Concrete Components provide default implementations of the operations.
+class ConcreteComponent : Component
+{
+    public override string Operation()
+    {
+        return "ConcreteComponent";
+    }
+}
+
+// The base Decorator class follows the same interface as the other
+// components.
+abstract class Decorator : Component
+{
+    protected Component _component;
+
+    public Decorator(Component component)
+    {
+        this._component = component;
+    }
+
+    public void SetComponent(Component component)
+    {
+        this._component = component;
+    }
+
+    // The Decorator delegates all work to the wrapped component.
+    public override string Operation()
+    {
+        if (this._component != null)
+        {
+            return this._component.Operation();
+        }
+        else
+        {
+            return string.Empty;
+        }
+    }
+}
+
+// Concrete Decorators call the wrapped object and alter its result in some
+// way.
+class ConcreteDecoratorA : Decorator
+{
+    public ConcreteDecoratorA(Component comp) : base(comp)
+    {
+    }
+
+    // Decorators may call parent implementation of the operation, instead
+    // of calling the wrapped object directly.
+    public override string Operation()
+    {
+        return $"ConcreteDecoratorA({base.Operation()})";
+    }
+}
+
+// Decorators can execute their behavior either before or after the call to
+// a wrapped object.
+class ConcreteDecoratorB : Decorator
+{
+    public ConcreteDecoratorB(Component comp) : base(comp)
+    {
+    }
+
+    public override string Operation()
+    {
+        return $"ConcreteDecoratorB({base.Operation()})";
+    }
+}
+
+public class Client
+{
+    // The client code works with all objects using the Component interface.
+    public void ClientCode(Component component)
+    {
+        Console.WriteLine("RESULT: " + component.Operation());
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Client client = new Client();
+
+        var simple = new ConcreteComponent();
+        Console.WriteLine("Client: I get a simple component:");
+        client.ClientCode(simple);
+        Console.WriteLine();
+        
+        ConcreteDecoratorA decorator1 = new ConcreteDecoratorA(simple);
+        ConcreteDecoratorB decorator2 = new ConcreteDecoratorB(decorator1);
+        Console.WriteLine("Client: Now I've got a decorated component:");
+        client.ClientCode(decorator2);
+    }
+}
